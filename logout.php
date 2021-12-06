1e69508896301de89b7cced318e0d4097ae28cc4
hafidz firman <91534713+hafidzfirman@users.noreply.github.com>
2021-12-06 13:51:08 +0700

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,9 +1,15 @@
 <?php
+// cek session
 session_start();
+
+// jadikan nilai session kosong terlebih dahula
 $_SESSION = [];
+
+// Lalu Hancurkan session tersebut
 session_unset();
 session_destroy();
 
+// Kembalikan ke halaman Login Lagi untuk bisa masuk ke Halaman Utama
 header("Location:Login.php");
 exit;
 ?>
